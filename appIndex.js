3373e576e102371f1455fb92ee4868a926e5ab85
Ruan Kenedi <ruanv1d4@gmail.com>
2018-01-10 17:24:33 -0200

    Added changes

--- appIndex.js
+++ appIndex.js
@@ -13,7 +13,7 @@ import './src/js/carousel';
 import './src/js/scrollIt';
 import './src/js/modal';
 import './src/js/slick.min';
-// import 'mediaelement';
+
 import 'mediaelement/build/mediaelement-and-player.min';
 import 'mediaelement/build/mediaelementplayer.css';
 import 'mediaelement/build/mejs-controls.svg';
@@ -40,136 +40,6 @@ function isMobile (a) {
   else false;
 }
 
-const player = new MediaElementPlayer('video-android', {
-	clickToPlayPause: true
-});
-
-console.log(player);
-
-// function isFooterVisible() {
-//   if (footer.isOnScreen()) {
-//     $('.about').removeClass('active');
-//     $('.contact').addClass('active');
-//   }
-// }
-
-// function isVehicleVisible() {
-//   if (hasCarStarted === false) {
-//     leftPlaceIcon.velocity({ opacity: 0 }, { duration: 0 });
-//   }
-
-//   if (hasTruckStarted === false) {
-//     rightPlaceIcon.velocity({ opacity: 0 }, { duration: 0 });
-//   }
-
-//   if ($('.car').isOnScreen() && hasCarStarted === false) {
-//     hasCarStarted = true;
-//     car.velocity({
-//       translateX: '285px'
-//     }, {
-//       duration: 0,
-//       begin: () => { dirtyCar = true; },
-//       complete: () => {
-//         dirtyCar = false;
-
-//         setTimeout(() => {
-//           leftPlaceIcon.velocity({ opacity: .1 }, { duration: 0 });
-//           leftPlaceIcon.velocity({ opacity: .5 }, { duration: 0 });
-//           leftPlaceIcon.velocity({ opacity: 1 }, { duration: 0 });
-//           leftPlaceIcon.addClass('bounceInDown');
-//         }, 580);
-//       }
-//     });
-//     carTier.velocity({
-//       translateX: '285px',
-//       rotateZ: '600deg'
-//     }, {
-//       duration: 0
-//     });
-
-//     return;
-//   }
-
-//   if (dirtyCar === false && $('.car').isOnScreen() === false) {
-//     hasCarStarted = false;
-//     dirtyCar = true;
-
-//     car.velocity({
-//       translateX: '0px'
-//     }, {
-//       duration: 300
-//     });
-//     carTier.velocity({
-//       translateX: '0px',
-//       rotateZ: '0deg'
-//     }, {
-//       duration: 300
-//     });
-//     leftPlaceIcon.removeClass('bounceInDown');
-
-//     return;
-//   }
-
-//   if ($('.truck').isOnScreen() && hasTruckStarted === false) {
-//     hasTruckStarted = true;
-
-//     truck.velocity({
-//       translateX: '-285px'
-//     }, {
-//       duration: 0,
-//       begin: () => { dirtyTruck = true; },
-//       complete: () => {
-//         dirtyTruck = false;
-
-//         setTimeout(() => {
-//           rightPlaceIcon.velocity({ opacity: .1 }, { duration: 0 });
-//           rightPlaceIcon.velocity({ opacity: .5 }, { duration: 0 });
-//           rightPlaceIcon.velocity({ opacity: 1 }, { duration: 0 });
-//           rightPlaceIcon.addClass('bounceInDown');
-//         }, 580);
-//       }
-//     });
-//     truckTier.velocity({
-//       translateX: '-285px',
-//       rotateZ: '-600deg'
-//     }, {
-//       duration: 0
-//     });
-//     secTruckTier.velocity({
-//       translateX: '-285px',
-//       rotateZ: '-600deg'
-//     }, {
-//       duration: 0
-//     });
-
-//     return;
-//   }
-
-//   if (dirtyTruck === false && $('.truck').isOnScreen() === false) {
-//     hasTruckStarted = false;
-//     dirtyTruck = true;
-
-//     truck.velocity({
-//       translateX: '0px'
-//     }, {
-//       duration: 620
-//     });
-//     truckTier.velocity({
-//       translateX: '0px',
-//       rotateZ: '0deg'
-//     }, {
-//       duration: 620
-//     });
-//     secTruckTier.velocity({
-//       translateX: '0px',
-//       rotateZ: '0deg'
-//     }, {
-//       duration: 620
-//     });
-//     rightPlaceIcon.removeClass('bounceInDown');
-//   }
-// }
-
 (function init($) {
   $(function () {
     $(document).ready(function () {
@@ -177,7 +47,29 @@ console.log(player);
         topOffset = -25;
 
         $('#left-banner').removeClass('m-tb-16');
-      }
+			}
+			
+			const player = new MediaElementPlayer('video-android', {
+				clickToPlayPause: true
+			});
+
+			const mediaAndroid = $('#mediaAndroid');
+			let displayControl = true;
+
+			const firstPlay = (event) => {
+				if (displayControl) {
+					event.currentTarget.style.opacity = '0';
+					
+					player.play();
+				}
+
+				displayControl = false;
+
+				mediaAndroid.off('click', firstPlay);
+				mediaAndroid.remove();
+			}
+
+			mediaAndroid.click(firstPlay);
 
       $('.carousel.carousel-slider').Carousel({
         fullWidth: true,
